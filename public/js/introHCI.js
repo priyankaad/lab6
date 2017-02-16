f837effba9b291ec5ce35928843a94df7cc447ac
Priyanka Dasgupta <pdasgupt@ucsd.edu>
2017-02-16 10:24:56 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -18,15 +18,41 @@ function initializePage() {
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
+
 	// Prevent following the link
 	e.preventDefault();
 
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
+
+
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+
 	console.log("User clicked on project " + idNumber);
+	var url= "/project/" +idNumber;
+
+	console.log(url);
+
+	$.get(url,callback);
+
+
+function callback(result){
+		console.log(result);
+		var projectHTML = '<a href="#">' +
+		 '<img src="' + result['image'] + '" class="detailsImage"' +
+		 '<p>' + result['title'] + '</p>' + '<p>' + 
+		 result['date'] + '</p></a>';
+
+		var currentPosition = "#project" + idNumber;
+
+		// $(currentPosition).html(projectHTML);
+		$(currentPosition + " .thumbnail .details").html(projectHTML+result['summary']);
+	}
+
+
+
 }
 
 /*
@@ -35,4 +61,21 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+    e.preventDefault();
+
+	$.get("/palette", callback);
+
+
+	function callback(data){
+		console.log(data["colors"]);
+		var colorArr = data["colors"];
+		var colors = colorArr["hex"];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	}
+
 }
